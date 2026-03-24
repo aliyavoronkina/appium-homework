568ea5a1f07b397a67a0b02b9e30e1c6f707dffa
Aliya Voronkina <atleugabilova@mail.ru>
2026-03-24 19:27:41 +0500

    Add Appium tests for Android app

---
+++ wdio.conf.js
@@ -0,0 +1,25 @@
+﻿exports.config = {
+    runner: 'local',
+    specs: ['./test/specs/**/*.js'],
+    capabilities: [{
+        platformName: 'Android',
+        'appium:deviceName': 'Samsung',
+        'appium:udid': 'R5CY648GZEF',
+        'appium:appPackage': 'ru.netology.testing.uiautomator',
+        'appium:appActivity': '.MainActivity',
+        'appium:automationName': 'UiAutomator2',
+        'appium:noReset': true
+    }],
+    services: ['appium'],
+    appium: {
+        command: 'appium',
+        logPath: './logs'
+    },
+    port: 4723,
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+    logLevel: 'info'
+};
